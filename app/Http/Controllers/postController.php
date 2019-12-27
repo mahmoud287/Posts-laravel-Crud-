bff2c1c060d4db2eb1b3112a30f1e134c6516209
mahmoud sherif <sherifmahmoud928@gmail.com>
2019-12-27 15:12:19 +0200

    pushing project

---
+++ app/Http/Controllers/postController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\post;
+use Illuminate\Http\Request;
+
+class postController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = post::latest()->paginate(5);
+
+        return view('posts.index', compact('posts'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+        post::create($request->all());
+
+        return redirect()->route('posts.index')
+            ->with('success', 'post created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(post $post)
+    {
+        return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, post $post)
+    {
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        $post->update($request->all());
+
+        return redirect()->route('posts.index')
+            ->with('success', 'post updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(post $post)
+    {
+        $post->delete();
+
+        return redirect()->route('posts.index')
+            ->with('success', 'post deleted successfully');
+    }
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,51 @@
+@extends('posts.layout')
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Posts Curd laravel 6</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-success" href="{{ route('posts.create') }}"> Create New Product</a>
+        </div>
+    </div>
+</div>
+
+@if ($message = Session::get('success'))
+<div class="alert alert-success">
+    <p>{{ $message }}</p>
+</div>
+@endif
+
+<table class="table table-bordered">
+    <tr>
+        <th>No</th>
+        <th>Title</th>
+        <th>Description</th>
+        <th width="280px">Action</th>
+    </tr>
+    @foreach ($posts as $post)
+    <tr>
+        <td>{{ ++$i }}</td>
+        <td>{{ $post->title }}</td>
+        <td>{{ $post->body }}</td>
+        <td>
+            <form action="{{ route('posts.destroy',$post->id) }}" method="POST">
+
+                <a class="btn btn-info" href="{{ route('posts.show',$post->id) }}">Show</a>
+
+                <a class="btn btn-primary" href="{{ route('posts.edit',$post->id) }}">Edit</a>
+
+                @csrf
+                @method('DELETE')
+
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+</table>
+
+{{-- paganation --}}
+{!! $posts->links() !!}
+@endsection
